f11f19bbc74b58daca59230df5d86e73e2964d02
globemediaofficial <109893647+globemediaofficial@users.noreply.github.com>
2025-06-26 00:26:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post("/process-video", upload.single("video"), (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8910;
 
 app.listen(PORT, () => {
   console.log("Server running on port 3000");
